b9ce35f2bd2237c899e69eba3c8620dd1292f495
Alejandro Suaza Builes <alejandrosuaza.1022@gmail.com>
2022-03-16 17:03:25 -0500

    form created

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,9 @@ import App from './App'
 import Register from './Register'
 import 'flowbite' 
 
-//const [isActive, setIsActive] = useState(true)
-
-function isActive2(){
-  const element = (false) ? <App /> : <Register />
-  return ReactDOM.render(element,document.getElementById("root"))
-}
-
-isActive2()
+ReactDOM.render(
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
