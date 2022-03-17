66734e9fcbb3de8c7552d97d12f9c8c3448e4568
Brayan Vargas <Brayan.Vargas@endava.com>
2022-03-17 09:21:35 -0500

    no change

--- src/UserRegisterForm.tsx
+++ src/UserRegisterForm.tsx
@@ -13,7 +13,7 @@ function UserRegisterForm() {
         <form>
           <div className="grid xl:grid-cols-2 xl:gap-6">
             <div className="relative z-0 mb-6 w-full group">
-              <label htmlFor="document_type" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select your document type</label>
+              <label htmlFor="document_type" className="block mb-2 text-sm font-medium text-gray-500 dark:text-gray-400">Select your document type</label>
               <select name="document_type" id="document_type" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option>CC</option>
                 <option>CE</option>
